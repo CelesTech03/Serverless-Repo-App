7bf81f8bc2ac686bd1d3a2fd6456d647ca8c8d70
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-06-08 00:14:04 -0400

    Max Element

--- max_nums.js
+++ max_nums.js
@@ -1,6 +1,6 @@
 // start coding your function here!
+// Function that takes in an array as a parameter and returns the element with the maximum value
 function find_max(arr) {
     return Math.max(...arr);
 }
-
 exports.find_max = find_max
