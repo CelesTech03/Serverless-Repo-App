e53549b989772df506d0c5bcdc2d334284f6325a
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-06-08 00:12:32 -0400

    Time Function

--- late.js
+++ late.js
@@ -1,4 +1,5 @@
 // start coding your function here!
+// Function to check if the time is at or after 10:00 PM
 function running_late(date){
     if(new Date(date).getHours() >= 22){
         return "It is late!";
