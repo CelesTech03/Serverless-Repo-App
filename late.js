ecee9b3f2b310c2fd9552a3abbbe52764d852da9
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-06-07 23:43:39 -0400

    update

--- late.js
+++ late.js
@@ -1,11 +1,10 @@
 // start coding your function here!
 function running_late(date){
-    date = new Date(date);
-    if (date.getHours() >= 22){
-        return "It is late!"
+    if(new Date(date).getHours() >= 22){
+        return "It is late!";
     } 
     else {
-        return "It is still early!"
+        return "It is still early!";
     }
 }
 exports.running_late = running_late;
