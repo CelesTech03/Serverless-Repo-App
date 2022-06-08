b9db120cf4092008998684b16dbb43ea3348e5d1
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-06-07 22:41:08 -0400

    Update late.js

--- late.js
+++ late.js
@@ -1,1 +1,11 @@
 // start coding your function here!
+function running_late(date){
+    if (date > 10){
+        return "It is late!"
+    } 
+    else {
+        return "It is still early!"
+    }
+}
+
+exports.running_late = running_late
